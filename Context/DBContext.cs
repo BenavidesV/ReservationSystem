f14918b78bbf027a11597313067c239bbd2ce778
Jose <benavides@estudiantec.cr>
2021-01-30 17:44:27 -0600

    initial commit

---
+++ Context/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Text;
+
+namespace WpfApp1.Context
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext()
+            : base("name=reservationEntities")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.Models
+{
+    class Reservation
+    {
+        public string id { get; set; }
+        public DateTime checkIn { get; set; }
+        public DateTime checkOut { get; set; }
+        public int nights { get; set; }
+        public string client { get; set; }
+        public string contractType { get; set; }
+        public float dollarsRate { get; set; }
+        public float colonsRate { get; set; }
+
+        public string observations { get; set; }
+    }
+}
